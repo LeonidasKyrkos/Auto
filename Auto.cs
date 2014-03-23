3c2026880fbbb4d50ade0b549c4208c1e094aac1
LeonidasKyrkos <leo.kyrkos@gmail.com>
2014-03-23 13:03:09 +0000

    first commit

---
+++ Auto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUTO
+{
+    class Auto
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+        public string Color;
+        public int Mileage;
+        public int Speed;
+        public int Speedlimit;
+
+        public void Accelerate()
+
+        {
+
+            Speed++;
+            if (Speed > Speedlimit)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Current speed: " + Speed + "        SLOW DOWN!!!!!");
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Current speed: " + Speed);
+            }
+        
+        }
+
+        public void Decelerate()
+
+        {
+            Speed--;
+            if (Speed > Speedlimit)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Current speed: " + Speed + "        SLOW DOWN!!!!!");
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Current speed: " + Speed);
+            }
+
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto myCar = new Auto();
+            myCar.Speed = 0;
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1988;
+            myCar.Color = "Silver";
+            myCar.Mileage = 20000;
+            myCar.Speedlimit = 50;
+
+            Console.WriteLine(myCar.Make + " " + myCar.Model);
+            Console.WriteLine("");
+
+            do
+            {
+
+                myCar.Accelerate();
+
+            }
+
+            while (myCar.Speed < 100);
+
+            if (myCar.Speed > myCar.Speedlimit)
+
+                {
+                     
+                     do
+                     {
+                         myCar.Decelerate();
+                     }
+
+                     while (myCar.Speed > 30);
+                }
+
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("My Current Speed: " + myCar.Speed);          
+            Console.ReadLine();
+
+        }
+    }
+}
